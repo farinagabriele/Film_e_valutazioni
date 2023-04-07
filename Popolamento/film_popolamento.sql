6da354cdac10b057032f271bc86178b634c7cf00
Valerio Savo <46973674+savobully@users.noreply.github.com>
2023-04-07 11:52:26 +0200

    Update film_popolamento.sql

--- Popolamento/film_popolamento.sql
+++ Popolamento/film_popolamento.sql
@@ -2,5 +2,5 @@ INSERT INTO Film (cod_film, titolo, regista, datauscita, produttore)
 VALUES ('A0234', 'Titanic', 'James Cameron', '1998-01-16', '20th Century Fox'),
        ('F0934', 'Fight Club', 'David Fincher', '1999-10-29', '20th Century Fox'),
        ('G0238', 'Quei bravi ragazzi', 'Martin Scorsese', '1990-09-20', 'Warner Bros'),
-       ('Z0264', 'La Haine', 'Mathieu Kassovitz', '1995-05-31', 'Studio Canal'),
-       ('A0731', 'L'allenatore nel pallone', 'Sergio Martino', '1984-10-26', 'Dania Film');
+       ('Z0264', "L'Odio", 'Mathieu Kassovitz', '1995-05-31', 'Studio Canal'),
+       ('A0731', "L'allenatore nel pallone", 'Sergio Martino', '1984-10-26', 'Dania Film');
