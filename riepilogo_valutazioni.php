16a6a10314608b0efc29fbb55a9f1524eb1d53e0
Gabriele Farina <skype.farina@gmail.com>
2023-04-07 12:10:58 +0200

    Compito 2 Cors

--- riepilogo_valutazioni.php
+++ riepilogo_valutazioni.php
@@ -12,7 +12,7 @@
         // Lista delle valutazioni
         include "connessione.php";
         // Query: elenco di tutte le valutazioni
-        $sql = "SELECT * FROM valutazione";
+        $sql = "SELECT * FROM Valutazioni;";
         $result = $con->query($sql);
         // Controllo
         if ($result->num_rows > 0)
@@ -32,11 +32,11 @@
                 echo "<tr>";
                 // Colonne
                 echo "<td>" . $row["id_valutazione"] ."</td>";
-                echo " <td> " . $row[" valutazione"] ." </td> ";
-                echo " <td> " . $row[" commento "] ." </td> ";
-                echo " <td> " . $row[" data_e_ora "] ." </td> ";
+                echo " <td> " . $row["valutazione"] ." </td> ";
+                echo " <td> " . $row["commento"] ." </td> ";
+                echo " <td> " . $row["data_e_ora"] ." </td> ";
                 echo " <td> " . $row["id_utente"]."</td>";
-                echo " <td> " . $row[" cod_film "] ." </td> ";
+                echo " <td> " . $row["cod_film"] ." </td> ";
 
 
 
@@ -46,12 +46,41 @@
             echo "</thead>";
             echo "</table>";
         }
-        // Media delle valutazioni
 
+        // Media delle valutazioni
+        $sql = "SELECT AVG(valutazione) AS media
+                FROM Valutazioni;";
+        $result = $con->query($sql);
+
+        // Controllo
+        if ($result->num_rows > 0)
+        {
+            $row = $result->fetch_assoc();
+            $media = $row["media"];
+            // Stampa la media
+            echo "<h3></h3>";
+        }
 
         // Il primo film con la valutazione massima
+        
+        $sql = "SELECT Film.titolo
+                FROM Film
+                INNER JOIN Valutazioni ON Valutazioni.cod_film = Film.cod_film
+                WHERE Valutazioni.valutazione = (SELECT MAX(valutazione)
+                                                 FROM Valutazioni)
+                LIMIT 1;";
+        $result = $con->query($sql);
 
+        if ($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+            $film_massimo = $row["titolo"];
+            // Stampa il film
+            echo "<h3></h3>";
+        }
 
+        // Chiusura connessione
+        $result->free();
+        $con->close();
     ?>
 </body>
 </html>
