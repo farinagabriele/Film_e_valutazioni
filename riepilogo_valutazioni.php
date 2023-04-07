7f100fc0ca5427c272ae7e3fcd25ad6af36b81e1
Gabriele Farina <skype.farina@gmail.com>
2023-04-07 11:27:13 +0200

    compito cors

---
+++ riepilogo_valutazioni.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riepilogo valutazioni</title>
+</head>
+<body>
+    <h1>Riepilogo valutazioni</h1>
+    <?php
+        // Lista delle valutazioni
+        include "connessione.php";
+        // Query: elenco di tutte le valutazioni
+        $sql = "";
+        $result = $con->query($sql);
+        // Controllo
+        if ($result->num_rows > 0)
+        {
+            // Stampa dell'output in formato tabellare
+            echo "<table class=\"table\">";
+            echo "<thead>";
+            echo "<th scope=\"col\">id_valutazione</th>";
+            echo "<th scope=\"col\">valutazione</th>";
+            echo "<th scope=\"col\">commento</th>";
+            echo "<th scope=\"col\">data_e_ora</th>";
+            echo "<th scope=\"col\">id_utente</th>";
+            echo "<th scope=\"col\">cod_film</th>";
+            echo "<tbody>";
+            while ($row = $result->fetch_assoc())
+            {
+                echo "<tr>";
+                // Colonne
+                echo "<td>" . $row["id_valutazione"] ."</td>";
+
+
+
+                echo "</tr>";
+            }
+            echo "</tbody>";
+            echo "</thead>";
+            echo "</table>";
+        }
+        // Media delle valutazioni
+
+
+        // Il primo film con la valutazione massima
+
+
+    ?>
+</body>
+</html>
